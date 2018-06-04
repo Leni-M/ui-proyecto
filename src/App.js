2c3126029fb349b9d8f76bae0859d803692e06e0
Leni <leni@example.com>
2018-06-03 21:54:27 -0400

    three change ui

--- src/App.js
+++ src/App.js
@@ -2,31 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Navigation from './components/Navigation';
+import {tareas} from './tasks.json';
+
+console.log(tareas);
 
 class App extends Component {
-    
-constructor(){
-    super();
-    this.state={
-        title:'Aplication de Tareas',
-        ntareas:10
+    constructor(){
+        super();
+        this.state={            
+            tareas
+        }
     }
-}
     
+
   render() {
+     const tasksAll= this.state.tareas.map((tarea,i)=>{
+         return(
+            <div className="card">
+             {tarea.title}
+            </div>
+         )
+         
+     })
+      
     return (
-      <div className="App">
+        <div className="App">
          <nav className="navbar navbar-dark bg-dark">
             <a href="" className="text-white">
-                {this.state.title}
+                Tasks
             </a>     
          </nav>
-        
+            {tasksAll}
          <img src={logo} className="App-logo" alt="logo" />
 
       </div>
-    );
+    
+    );        
+   
   }
 }
 
